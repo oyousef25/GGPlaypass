e3140a083a8bc20c32b1d25d7c08e35ed6c7e328
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2025-04-07 13:35:33 -0400

    Fixed some build issues

--- gg-play-pass/src/App.tsx
+++ gg-play-pass/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
