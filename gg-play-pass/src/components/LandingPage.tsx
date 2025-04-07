6db5568bd9d0257db63c91c0462c36e0bda79e34
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2025-04-07 15:58:46 -0400

    video fixes

--- gg-play-pass/src/components/LandingPage.tsx
+++ gg-play-pass/src/components/LandingPage.tsx
@@ -42,6 +42,7 @@ const LandingPage = () => {
           <video
             autoPlay
             loop
+            playsInline
             muted
             style={{
               width: '100%',
