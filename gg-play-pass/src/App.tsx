97fe9db4e3c1af5bddcc740a536e8aa12901746b
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2025-04-07 11:58:38 -0400

    Setup React Router

--- gg-play-pass/src/App.tsx
+++ gg-play-pass/src/App.tsx
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="*" element={<div>404 Not Found</div>} /> {/* Fallback for unmatched routes */}
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
